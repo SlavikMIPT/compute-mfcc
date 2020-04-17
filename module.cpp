1680c36ae423f15ee381ec1bf01e85ee35a22f0c
SlavikMIPT <slavikmipt@gmail.com>
2020-04-17 08:28:48 +0300

    refactor

--- module.cpp
+++ module.cpp
@@ -13,22 +13,23 @@ py::list process_impl(py::list &inList, int &sampFreq, int &nCep,
       static_cast<unsigned int>(kMFCCFrameShift * 512 / 96.0);
   py::list &li = inList;
   py::list lo;
-  Math::v_d_t frame, out_vector;
+  Math::v_d_t out_vector;
+  int16_t *tmpbuf = new int16_t[WINDOW_SIZE];
   Math::MFCC *mfcc_instance =
       new Math::MFCC(sampFreq, nCep, winLength, frameShift, numFilt, lf, hf);
   for (int i = 0; i <= inList.size() - WINDOW_SIZE; i += HOP_SIZE) {
-    frame.clear();
     for (int j = i; j < i + WINDOW_SIZE; j++) {
-      frame.push_back(li[j].cast<double>());
+      tmpbuf[j - i] = py::cast<int16_t>(li[j]);
     }
     out_vector.clear();
-    mfcc_instance->processFrame<Math::v_d_t>(frame, out_vector);
+    mfcc_instance->processFrame<Math::v_d_t>(tmpbuf, WINDOW_SIZE, out_vector);
     py::list tmp;
     for (auto it = out_vector.cbegin(); it != out_vector.cend(); ++it) {
       tmp.append(py::float_(*it));
     }
     lo.append(tmp);
   }
+  delete[] tmpbuf;
   delete mfcc_instance;
   return lo;
 }
